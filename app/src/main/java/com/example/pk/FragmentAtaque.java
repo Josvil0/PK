03f3ce8417eee1ed6c6d8dcb4c44a14a1875586d
josvil0 <jalonsovillanova0@gmail.com>
2024-11-22 20:37:11 +0100

    Añadido menu inferior(falta implementar accion botones)

--- app/src/main/java/com/example/pk/FragmentAtaque.java
+++ app/src/main/java/com/example/pk/FragmentAtaque.java
@@ -2,22 +2,18 @@ package com.example.pk;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmenMain#newInstance} factory method to
+ * Use the {@link FragmentAtaque#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmenMain extends Fragment {
+public class FragmentAtaque extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,9 +24,7 @@ public class FragmenMain extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private Button botonAtaque;
-
-    public FragmenMain() {
+    public FragmentAtaque() {
         // Required empty public constructor
     }
 
@@ -40,11 +34,11 @@ public class FragmenMain extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmenMain.
+     * @return A new instance of fragment FragmentAtaque.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmenMain newInstance(String param1, String param2) {
-        FragmenMain fragment = new FragmenMain();
+    public static FragmentAtaque newInstance(String param1, String param2) {
+        FragmentAtaque fragment = new FragmentAtaque();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,29 +59,6 @@ public class FragmenMain extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmen_main, container, false);
-    }
-
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        botonAtaque = view.findViewById(R.id.botonAtacar);
-
-        botonAtaque.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentAtaque fragmentAtaque = new FragmentAtaque();
-
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-
-                transaction.replace(R.id.menu,fragmentAtaque);
-                transaction.addToBackStack(null);
-
-
-                transaction.commit();
-            }
-        });
+        return inflater.inflate(R.layout.fragment_ataque, container, false);
     }
 }
